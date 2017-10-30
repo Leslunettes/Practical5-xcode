1b3ca63048a737fe1dfd0cbd1cd8f1fae765e839
LesLunettes <adrienpourtier@orange.fr>
2017-10-30 19:09:28 +0000

    Commit all

--- Practical5-xcode/main.cpp
+++ Practical5-xcode/main.cpp
@@ -1,15 +1,70 @@
-//
-//  main.cpp
-//  Practical5-xcode
-//
-//  Created by Adrien Pourtier on 30/10/2017.
-//  Copyright © 2017 Adrien Pourtier. All rights reserved.
-//
+/*
+ * main.cpp
+ */
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include "ShapeTester.h"
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char* argv[]) {
+
+	if (false) {
+		for (unsigned int i=0 ; i<1 ; ++i) {
+			ShapeTester t;
+		}
+		return 0;
+
+	}
+
+	while (argc > 1) {
+
+		switch (argv[--argc][0]) {
+		case 'a':
+		{
+			ShapeTester t;
+			t.testCtor();
+		}
+		break;
+		case 'b':
+		{
+			ShapeTester t;
+			t.testSimpleFunctions();
+		}
+		break;
+		case 'c':
+		{
+			ShapeTester t;
+			t.testArea();
+		}
+		break;
+		case 'd':
+		{
+			ShapeTester t;
+			t.testPerimeter();
+		}
+		break;
+		case 'e':
+		{
+			ShapeTester t;
+			t.testToString();
+		}
+		break;
+		case 'f':
+		default:
+		{
+			ShapeTester t;
+			t.testOstreamRedir();
+		}
+		break;
+		}
+	}
+	return 0;
 }
+
